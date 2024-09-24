bc3cbaa52f95dbd973daa1ccb5870d64d546a1b0
Jaye <jaye.engelhardt@gmail.com>
2024-09-24 20:15:55 +0000

    modified:   Gemfile 	modified:   Gemfile.lock 	new file:   app/assets/stylesheets/header.css 	modified:   app/views/layouts/application.html.erb 	modified:   app/views/sessions/new.html.erb 	new file:   db/migrate/20240924185042_add_password_digest_to_users.rb 	new file:   db/migrate/20240924185100_add_password_digest_to_admins.rb 	modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_19_161057) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_24_185100) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_19_161057) do
     t.bigint "user_id", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "password_digest"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
     t.index ["user_id"], name: "index_admins_on_user_id"
@@ -71,6 +72,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_19_161057) do
     t.string "type"
     t.string "first_name"
     t.string "last_name"
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
